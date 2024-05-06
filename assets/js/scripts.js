ae67c38e7ee04194ce536d3d948d27ab6497f062
Priyanka Patel <priyanka.p@upcues.com>
2024-05-06 16:55:19 +0530

    ticket landing page elements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,6 +18,30 @@ $('#file-upload-3').change(function() {
     var filename = m[1];
     $('#filename-3').text(filename);
 });
+$('#file-upload-4').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-4').text(filename);
+});
+$('#file-upload-5').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-5').text(filename);
+});
+$('#file-upload-6').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-6').text(filename);
+});
+$('#file-upload-7').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-7').text(filename);
+});
 
 //CHECKBOX
 $(".add-after").after('<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.33366 2.5L3.75033 7.08333L1.66699 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>');
