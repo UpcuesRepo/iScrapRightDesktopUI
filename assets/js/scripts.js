2e69ab6ad1857146be1cdcbe33a833444bcc5df2
Priyanka Patel <priyanka.p@upcues.com>
2024-05-20 16:57:07 +0530

    floating select and modals

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,37 @@
+// ----floatin input label in input and select
+
+$('.form-group').find('.floating-control').each(function (index, ele) {
+	var $ele = $(ele);
+	if($ele.val() != '' || $ele.is(':selected') === true){
+		$ele.parents('.floating-group').addClass('focused');
+	}
+})
+
+
+$('.floating-control').on('focus', function (e) {
+	$(this).parents('.floating-group').addClass('focused');	
+}).on('blur', function(){
+	if($(this).val().length > 0){
+		$(this).parents('.floating-group').addClass('focused');		
+	}else{
+		$(this).parents('.floating-group').removeClass('focused');
+	}
+});
+$('.floating-control').on('change', function (e) {
+	if($(this).is('select')){
+		if($(this).val() === $("option:first", $(this)).val()) {
+			$(this).parents('.floating-group').removeClass('focused');
+		}
+		else{
+			$(this).parents('.floating-group').addClass('focused');
+		}
+	}
+})
+
+
+
+
+
 // BROWSE FILES
 
 $('#file-upload').change(function() {
@@ -48,23 +82,3 @@ $(".form-check .add-after").after('<svg width="10" height="10" viewBox="0 0 10 1
 $(".form-check.style-2 .add-after").after('<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.33366 2.5L3.75033 7.08333L1.66699 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>');
 // END CHECKBOX
 
-
-const getDatePickerTitle = elem => {
-    // From the label or the aria-label
-    const label = elem.nextElementSibling;
-    let titleText = '';
-    if (label && label.tagName === 'LABEL') {
-      titleText = label.textContent;
-    } else {
-      titleText = elem.getAttribute('aria-label') || '';
-    }
-    return titleText;
-  }
-  
-  const elems = document.querySelectorAll('.datepicker_input');
-  for (const elem of elems) {
-    const datepicker = new Datepicker(elem, {
-      'format': 'dd/mm/yyyy', // UK format
-      title: getDatePickerTitle(elem)
-    });
-  }
