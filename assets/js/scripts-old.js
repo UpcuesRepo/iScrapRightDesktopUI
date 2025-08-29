a7b63c77dbc8aba5e5c669c72a1d6fc93985df5b
Priyanka Patel <priyanka.p@upcues.com>
2025-08-29 11:41:57 +0530

    script file replace

---
+++ assets/js/scripts-old.js
@@ -0,0 +1,441 @@
+$(document).ready(function() {
+   $('#myTable').DataTable();  
+});
+// $(document).ready(function() {
+//    $('#subscriTable').DataTable();  
+// });
+
+$(document).ready(function(){
+        $('#myDatepicker').datepicker({
+            format: 'dd-mm-yyyy', // Define the date format
+            autoclose: true,     // Close the datepicker when a date is selected
+            todayHighlight: true // Highlight today's date
+        });
+});
+$(document).ready(function(){
+        $('#myDatepicker2').datepicker({
+            format: 'dd-mm-yyyy', // Define the date format
+            autoclose: true,     // Close the datepicker when a date is selected
+            todayHighlight: true // Highlight today's date
+        });
+});
+$(document).ready(function(){
+        $('.calender-simple').datepicker({
+            format: 'dd-mm-yyyy', // Define the date format
+            autoclose: true,     // Close the datepicker when a date is selected
+            todayHighlight: true // Highlight today's date
+        });
+});
+$(document).ready(function(){
+        $('.calender-rgba').datepicker({
+            format: 'dd-mm-yyyy', // Define the date format
+            autoclose: true,     // Close the datepicker when a date is selected
+            todayHighlight: true // Highlight today's date
+        });
+});
+
+// SUBSCRIPTION-NEW STEPS
+let currentStep = 0;
+    const steps = document.querySelectorAll('.step');
+    const contents = document.querySelectorAll('.content');
+
+    function updateStepper() {
+      steps.forEach((step, index) => {
+        step.classList.toggle('active', index <= currentStep);
+      });
+      contents.forEach((content, index) => {
+        content.classList.toggle('active', index === currentStep);
+      });
+    }
+
+    function nextStep() {
+      if (currentStep < steps.length - 1) {
+        currentStep++;
+        updateStepper();
+      }
+    }
+
+    function prevStep() {
+      if (currentStep > 0) {
+        currentStep--;
+        updateStepper();
+      }
+    }
+// END SUBSCRIPTION -NEW STEPS
+
+
+
+
+
+
+$(document).ready(function() {
+
+  //CHECKBOX
+$(".form-check .add-after").after('<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.33366 2.5L3.75033 7.08333L1.66699 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>');
+$(".form-check.style-2 .add-after").after('<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.33366 2.5L3.75033 7.08333L1.66699 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>');
+// END CHECKBOX
+
+//RADIO
+    $(".add-after2").after('<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="10" height="10" rx="5" transform="matrix(1 0 0 -1 5 15)" fill="#3384D8"/></svg>');
+// END RADIO
+
+  // SLICK
+$('.pricing-slide').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// END SLICK
+
+
+  //plus minus
+$('.btn-number').click(function(e){
+    e.preventDefault();
+    
+    fieldName = $(this).attr('data-field');
+    type      = $(this).attr('data-type');
+    var input = $("input[name='"+fieldName+"']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+        if(type == 'minus') {
+            
+            if(currentVal > input.attr('min')) {
+                input.val(currentVal - 1).change();
+            } 
+            if(parseInt(input.val()) == input.attr('min')) {
+                $(this).attr('disabled', true);
+            }
+
+        } else if(type == 'plus') {
+
+            if(currentVal < input.attr('max')) {
+                input.val(currentVal + 1).change();
+            }
+            if(parseInt(input.val()) == input.attr('max')) {
+                $(this).attr('disabled', true);
+            }
+
+        }
+    } else {
+        input.val(0);
+    }
+});
+$('.input-number').focusin(function(){
+   $(this).data('oldValue', $(this).val());
+});
+$('.input-number').change(function() {
+    
+    minValue =  parseInt($(this).attr('min'));
+    maxValue =  parseInt($(this).attr('max'));
+    valueCurrent = parseInt($(this).val());
+    
+    name = $(this).attr('name');
+    if(valueCurrent >= minValue) {
+        $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the minimum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+    if(valueCurrent <= maxValue) {
+        $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the maximum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+    
+    
+});
+$(".input-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+//end plus minus
+
+    // ----floating input label in input and select
+
+    $('.form-group').find('.floating-control').each(function (index, ele) {
+      var $ele = $(ele);
+      if($ele.val() != '' || $ele.is(':selected') === true){
+        $ele.parents('.floating-group').addClass('focused');
+      }
+    })
+    
+    
+    
+    
+    $('.floating-control').on('focus', function (e) {
+      $(this).parents('.floating-group').addClass('focused');	
+    }).on('blur', function(){
+      if($(this).val().length > 0){
+        $(this).paren
+    ts('.floating-group').addClass('focused');		
+      }else{
+        $(this).parents('.floating-group').removeClass('focused');
+      }
+    });
+    $('.floating-control').on('change', function (e) {
+      if($(this).is('select')){
+        if($(this).val() === $("option:first", $(this)).val()) {
+          $(this).parents('.floating-group').removeClass('focused');
+        }
+        else{
+          $(this).parents('.floating-group').addClass('focused');
+        }
+      }
+    })
+
+
+
+
+
+
+      //---- select2 single----
+      $('.customSelect').each(function() {
+        var dropdownParents = $(this).parents('.select2Part')
+        $(this).select2({
+          dropdownParent: dropdownParents,
+          minimumResultsForSearch: -1
+        }).on("select2:open", function (e) { 
+          $(this).parents('.floating-group').addClass('focused');
+        }).on("select2:close", function (e) {
+          if($(this).find(':selected').val() === ''){
+            $(this).parents('.floating-group').removeClass('focused');
+          }
+        });
+    });
+
+// PRICING STEPS
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const nextBtnFourth = document.querySelector(".next-3");
+const prevBtnFourth = document.querySelector(".prev-3");
+const nextBtnFifth = document.querySelector(".next-4");
+const prevBtnFifth = document.querySelector(".prev-4");
+const prevBtnSixth = document.querySelector(".prev-5");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const step = document.querySelectorAll(".step");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-20%";
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-100%";
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnFifth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-125%";
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  step[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
+});
+
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  step[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-20%";
+  step[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  step[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFifth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  step[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnSixth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-100%";
+  step[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+// END PRICING STEPS
+
+
+});
+
+
+
+// BROWSE FILES
+
+$('#file-upload').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename').text(filename);
+});
+$('#file-upload-2').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-2').text(filename);
+});
+$('#file-upload-3').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-3').text(filename);
+});
+$('#file-upload-4').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-4').text(filename);
+});
+$('#file-upload-5').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-5').text(filename);
+});
+$('#file-upload-6').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-6').text(filename);
+});
+$('#file-upload-7').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-7').text(filename);
+});
+$('#file-upload-8').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename-8').text(filename);
+});
+
+
+
+///// Script For Height Calculation
+
+var mainHeight = $(window).innerHeight();
+var headerHeight = $('.ticket-landing-navbar').innerHeight();
+var FooterHeight = $('.ticket-footer').innerHeight();
+var innercontentHeight = mainHeight - (headerHeight + FooterHeight); 
+var skyblueHeight = $('#skyblue-strip-scroll').innerHeight();
+
+$('#content-part-scroll').css('height', mainHeight-(headerHeight + FooterHeight)+'px');
+$('#rightscroll').css('height', mainHeight- (headerHeight + FooterHeight + skyblueHeight)+'px');
+
+console.log(FooterHeight);
+console.log(headerHeight);
+console.log(innercontentHeight);
+console.log(mainHeight);
+console.log(skyblueHeight);
+
+window.onresize = function(event) {
+var mainHeight = $(window).innerHeight();
+var headerHeight = $('.ticket-landing-navbar').innerHeight();
+var FooterHeight = $('.ticket-footer').innerHeight();
+var innercontentHeight = mainHeight - (headerHeight + FooterHeight); 
+var skyblueHeight = $('#skyblue-strip-scroll').innerHeight();
+
+$('#content-part-scroll').css('height', mainHeight-(headerHeight + FooterHeight)+'px');
+$('#rightscroll').css('height', mainHeight-(headerHeight + FooterHeight + skyblueHeight)+'px');
+  
+  console.log(FooterHeight);
+  console.log(headerHeight);
+  console.log(innercontentHeight);
+  console.log(mainHeight);
+  console.log(skyblueHeight);
+
+};
+
+
